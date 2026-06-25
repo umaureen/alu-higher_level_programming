e1affc6b6bd1fc9923faebb2e059df391a3f5b4d
umaureen <u.maureen@alustudent.com>
2026-06-25 21:31:04 +0200

    echo

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -5,11 +5,8 @@ Python script that fetches https://alu-intranet.hbtn.io/status
 import requests
 
 if __name__ == '__main__':
-    url = 'https://intranet.hbtn.io/status'
+    url = 'https://alu-intranet.hbtn.io/status'
     
-    if url == 'https://intranet.hbtn.io/status':
-        url = "https://alu-intranet.hbtn.io/status"
-
     res = requests.get(url)
     print("Body response:")
     print("\t- type: {}".format(type(res.text)))
